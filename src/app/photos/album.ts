3f4b816c1c472f141c9f1a9752ec79e40ee76f2d
Venkatesh R <venkatesh.br@gmail.com>
2020-03-31 23:40:11 +0800

    Auto-generated commit

--- src/app/photos/album.ts
+++
@@ -1,8 +0,0 @@
-export interface Album{
-    id: string,
-    title: string,
-    productUrl: string,
-    mediaItemsCount: string,
-    coverPhotoBaseUrl: string,
-    coverPhotoMediaItemId: string
-}
